286c8fc1826bb5d1bf59b4257291fb1afe9986df
Mahntri <nmanhtri07@gmail.com>
2025-12-03 23:10:55 +0700

    update connect mongo

--- src/config/connectMongo.js
+++ src/config/connectMongo.js
@@ -1,13 +1,23 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
+let cachedConnection = null;
+
 const connectMongo = async () => {
+  if (cachedConnection) {
+    return cachedConnection;
+  }
+
   try {
-    await mongoose.connect(
-      process.env.MONGO_URL,
-    );
-    console.log("Connected to MongoDB successfully");
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    
+    console.log("New MongoDB Connection established");
+    
+    cachedConnection = conn;
+    
+    return conn;
   } catch (err) {
     console.error("Error connecting to MongoDB", err);
     process.exit(1);
@@ -15,5 +25,3 @@ const connectMongo = async () => {
 };
 
 export default connectMongo;
-
-
