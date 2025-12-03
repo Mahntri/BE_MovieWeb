e3fe89e7a99018ed6213099e8a98c78e1c2df2d8
Mahntri <nmanhtri07@gmail.com>
2025-12-03 13:02:14 +0700

    Fix CORS error and add vercel config

--- index.js
+++ index.js
@@ -9,10 +9,28 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://fe-movie-web-chi.vercel.app"
+];
+
 app.use(cors({
-    origin: "*",
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) === -1) {
+      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
+
+app.options('*', cors());
+
 app.use(express.json());
 
 connectMongo();
