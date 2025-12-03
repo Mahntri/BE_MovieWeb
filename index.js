1dbc40253f971376deb9258c9ed7fa43740b8bb6
Mahntri <nmanhtri07@gmail.com>
2025-12-03 16:51:39 +0700

    .

--- index.js
+++ index.js
@@ -9,28 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://fe-movie-web-chi.vercel.app"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
+    origin: "*",
+    credentials: true
 }));
-
-app.options('*', cors());
-
 app.use(express.json());
 
 connectMongo();
